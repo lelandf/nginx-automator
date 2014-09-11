fe98c48d6ce0f5dfceebd6869575b0555c67a641
lelandf <lelandf@gmail.com>
2014-09-10 20:00:09 -0400

    Add nginx wp automator

---
+++ nginx-wp.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+domain=$1
+root="/var/www/$domain/html"
+block="/etc/nginx/sites-available/$domain"
+
+# Create the Document Root directory
+sudo mkdir -p $root
+
+# Assign ownership to your regular user account
+sudo chown -R $USER:$USER $root
+
+# Create the Nginx server block file:
+sudo tee $block > /dev/null <<EOF 
+server {
+        listen 80;
+        listen [::]:80;
+
+        root /var/www/$domain/html;
+        index index.php index.html index.htm;
+
+        server_name $domain www.$domain;
+		include hhvm.conf;
+
+        location / {
+                try_files $uri $uri/ /index.php?q=$request_uri;
+        }
+}
+
+
+EOF
+
+# Link to make it available
+sudo ln -s $block /etc/nginx/sites-enabled/
+
+# Test configuration and reload if successful
+sudo nginx -t && sudo service nginx reload
